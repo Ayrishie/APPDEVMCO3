16fbac5b2110f1a9c042e77f62da583cf32b2634
Angelene Atienza <93639726+geleanne@users.noreply.github.com>
2023-12-02 10:09:06 +0800

    partial search works; improve handling duplicates

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -3,9 +3,9 @@ import reservationController from "../controllers/reservationController.js";
 import express from "express";
 
 const searchController = {
-    getSearch: async function (req, res) {
-        const searchText = req.query.building.toLowerCase();
-        const buildings = await Building.find({ buildingNameLower: { $regex: searchText, $options: 'i' } }).lean();
+    getSearch: async function(req, res){
+        const searchQuery = req.query.building.toLowerCase();
+        const buildings = await Building.find({ buildingNameLower: { $regex: searchQuery, $options: 'i' } }).lean();
 
         res.render("search", {
             css: "search",
