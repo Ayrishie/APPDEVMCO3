e029ef2b85ecd0cbd51fec3838599a0fba592fc6
Angelene Atienza <93639726+geleanne@users.noreply.github.com>
2023-12-02 09:57:27 +0800

    changes to the profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const editDescriptionButton = document.getElementById("edit-description-button");
-    const saveDescriptionButton = document.getElementById("save-description-button");
+    const editProfileButton = document.getElementById("edit-profile-button");
+    const saveProfileButton = document.getElementById("save-profile-button");
     const userDescription = document.getElementById("user-description");
     const editDescriptionTextarea = document.getElementById("edit-description");
     const profilePictureUpload = document.getElementById("profile-picture-upload");
@@ -8,22 +8,21 @@ document.addEventListener("DOMContentLoaded", function () {
     const navbarUserImage = document.querySelector(".user-image");
     const viewProfileButton = document.getElementById("view-profile-button");
 
-
-    // 1. Allow users to edit the description and display the change on the screen
-    if ($("#edit-description-button").length) {
-        editDescriptionButton.addEventListener("click", function () {
+    // 1. Allow users to edit the profile description and display the change on the screen
+    if ($("#edit-profile-button").length) {
+        editProfileButton.addEventListener("click", function () {
             userDescription.style.display = "none";
             editDescriptionTextarea.style.display = "block";
             editDescriptionTextarea.removeAttribute("readonly"); // Enable text input
-            saveDescriptionButton.style.display = "block";
+            saveProfileButton.style.display = "block";
             editDescriptionTextarea.value = userDescription.textContent.replace("Description: ", "");
         });
 
-        saveDescriptionButton.addEventListener("click", function () {
+        saveProfileButton.addEventListener("click", function () {
             userDescription.style.display = "block";
             editDescriptionTextarea.style.display = "none";
             editDescriptionTextarea.setAttribute("readonly", "readonly");
-            saveDescriptionButton.style.display = "none";
+            saveProfileButton.style.display = "none";
             const editedDescription = editDescriptionTextarea.value.trim();
             userDescription.textContent = "Description: " + (editedDescription !== "" ? editedDescription : "No description available");
 
@@ -42,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (profileImage) {
                     profileImage.src = imageUrl; // Update profile image
                 }
-                if (navbarUserImage) { // Check if navbarUserImage exists
+                if (navbarUserImage) {
                     navbarUserImage.src = imageUrl; // Update navbar user image
                 }
                 console.log("Profile picture updated:", imageUrl);
@@ -56,13 +55,12 @@ document.addEventListener("DOMContentLoaded", function () {
         profilePictureUpload.value = ""; // Clear the file input selection
     });
 
-    $(".delete-button").on("click", function(){
+    $(".delete-button").on("click", function () {
         $.ajax({
             url: "/profile/:email/deleteAccount",
-            method: "DELETE"
+            method: "DELETE",
         }).done(() => {
             window.location.href = "/";
         });
     });
-
 });
